16526fcf7e92275f7b7c00f68c23fef884af1228
nanthu1696 <118247818+nanthu1696@users.noreply.github.com>
2023-02-15 22:48:35 +0530

    Update history.js

--- history.js
+++ history.js
@@ -1,21 +1,19 @@
 let main2 = document.querySelector(".card2");
 const cards = JSON.parse(localStorage.getItem("list") || '[]');
 function display(){
-    console.log(cards);
     for (let i = 0; i < cards.length; i++){
         if(cards[i] == null)
         {
-            console.log("hi");
-            cards.splice(i,1);
-            localStorage.setItem("list", JSON.stringify(cards));
+            let arr = cards.splice(i,-1);
         }
         else{
+            console.log(cards[i]);
         let div = document.createElement("div");
         div.className = "newCard2";
         let innerDiv = document.createElement("div");
         innerDiv.innerHTML =`<p>${cards[i].operation} : ${cards[i].expression}</p> \n <p>Solution: ${cards[i].result}</p>`;
         let button = document.createElement("button");
-        button.innerHTML = "Delete<i class = 'fa fa-trash-o'></i>";
+        button.innerHTML = "<i class = 'fa fa-trash-o'></i>";
         div.appendChild(innerDiv);
         div.appendChild(button);
         main2.appendChild(div);
@@ -23,7 +21,6 @@ function display(){
             div.remove();
             delete cards[i];
             localStorage.setItem("list", JSON.stringify(cards));
-            console.log(cards);
         });
     }
     }
